4403b09f13d91790f4e493ecb9d5482f27e7e101
ellengiacometti <ellengiacometti@gmail.com>
2019-02-01 00:40:22 -0200

    ColorSegmentation Funcionando e Printando o contor;

--- ColorSegmentation.py
+++ ColorSegmentation.py
@@ -43,10 +43,10 @@ maskBrown = cv.inRange(hsv_image, light_brown, dark_brown)
 mask = maskGreen + maskYellow + maskBrown
 
 result = cv.bitwise_and(cv.cvtColor(image,cv.COLOR_BGR2RGB), cv.cvtColor(image,cv.COLOR_BGR2RGB), mask=mask)
-
+gray=cv.cvtColor(result,cv.COLOR_RGB2GRAY)
 # Clean up the segmentation using a blur
 blur = cv.GaussianBlur(result, (7, 7), 0)
-fig, (ax1, ax2,ax3) = plt.subplots(1, 3, figsize=(10, 5), sharex=True, sharey=True)
+fig, (ax1, ax2,ax3,ax4) = plt.subplots(1, 4, figsize=(10, 5), sharex=True, sharey=True)
 ax1.axis('off')
 ax1.imshow(cv.cvtColor(image,cv.COLOR_BGR2RGB))
 ax1.set_title(' RGB IMAGE')
@@ -56,8 +56,28 @@ ax2.set_title('HSV IMAGE')
 ax3.axis('off')
 ax3.imshow(blur, cmap=plt.cm.gray)
 ax3.set_title('RESULT IMAGE')
+ax4.axis('off')
+ax4.imshow(gray, cmap=plt.cm.gray)
+ax4.set_title('GRAY IMAGE')
 plt.show()
 
- contornos = sm.find_contours(result>0.01,0.19,fully_connected='low')
- tamanho_contorno =len(contornos)
- print("Número de Contornos Encontrados:", len(contornos))
+
+contornos = sm.find_contours(gray,0.19,fully_connected='low')
+tamanho_contorno =len(contornos)
+print("Número de Contornos Encontrados:", len(contornos))
+tamanho = np.empty([tamanho_contorno])
+for i in range(tamanho_contorno):
+    tamanho[i]=contornos[i].shape[0]
+#Detectando os N maiores contornos e seus indices, alterando N , os N maiores contornos serão desenhados
+maiores=nlargest(1,enumerate(tamanho),key=lambda a: a[1])
+print("MaiorContorno:", maiores)
+
+fig1, ax = plt.subplots()
+ax.imshow(result, interpolation='nearest', cmap=plt.cm.gray)
+for  n, contorno in enumerate(contornos):
+    if(n in (np.transpose(np.asanyarray(maiores))[0])):
+        ax.plot(contorno[:, 1], contorno[:, 0],'-b', linewidth=2)
+plt.show()
+
+##TODO:TROCAR FUNÇÃO DE CONTORNO DO SCIKIT PELA DO OPENCV
+##TODO: ACHAR A AREA DO CONTORNO E DE CADA MASK , USAR OU NÃO REGIONPROPS
