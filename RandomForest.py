6af4abd8a53a8dfb31d05406033fd3073a656508
ellengiacometti <ellengiacometti@gmail.com>
2019-02-13 02:09:04 -0200

    SVM  adaptado para pandas e com GRIDSEARCH

--- RandomForest.py
+++ RandomForest.py
@@ -15,17 +15,16 @@ if __name__ == '__main__':
     TextureLabel = train['TextureLabel']
     TextureLabelTest=test['TextureLabel']
     le = LabelEncoder()
-    # # Fit the encoder to the pandas column
     le.fit(TextureLabelTest)
     TextureLabelTest = le.transform(TextureLabelTest)
     TextureLabel = le.transform(TextureLabel)
 #class_weight={0:1,1:2} should do the job. Now, class 0 has weight 1 and class 1 has weight 2.
-##Create a based model
+# #Create a based model
 # rf = RandomForestClassifier(random_state=42)
 # # Create the parameter grid based on the results of random search
 # param_grid = {
-#     'n_estimators': [10, 20, 30, 40,50,60,70,80,90,100,150,200],
-#     'class_weight': [{0: w1,1: w2} for w1 in [2, 4, 6, 10,12] for w2 in [2, 4, 6, 10,12]]
+#     'n_estimators': [10, 20, 30, 40,50,60,70,80,90,100],
+#     'class_weight': [{0: 1,1: w2} for w2 in [2, 4, 6, 10,12]]
 #
 # }
 # # Instantiate the grid search model
@@ -40,7 +39,7 @@ if __name__ == '__main__':
 
 
 
-BEST_RFC=RandomForestClassifier(class_weight={0: 1, 1: 6}, n_estimators= 10)
+BEST_RFC=RandomForestClassifier(class_weight={0: 1, 1: 2}, n_estimators= 20)
 BEST_RFC.fit(train[Texture], TextureLabel)
 pred=BEST_RFC.predict(test[Texture])
 print("Accuracy for Random Forest on CV data: ",accuracy_score(TextureLabelTest,pred))
--- SVM.py
+++ SVM.py
@@ -3,81 +3,106 @@ import numpy as np
 import os
 import glob
 from TrataImagem import TrataImagem
+from sklearn.metrics import accuracy_score
 from sklearn.svm import SVC
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import cross_val_score
 from sklearn.preprocessing import StandardScaler
-from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+
 
 
 if __name__ == '__main__':
-    # load the training dataset
-    train_path  = "/home/ellengiacometti/PycharmProjects/TCCFRUIT/PIC_LM_LABEL"
-    train_names = os.listdir(train_path)
+    # # load the training dataset
+    # train_path  = "/home/ellengiacometti/PycharmProjects/TCCFRUIT/PIC_LM_LABEL"
+    # train_names = os.listdir(train_path)
+    #
+    # # empty list to hold feature vectors and train labels
+    # train_features = []
+    # train_labels = []
+    #
+    # # loop over the training dataset
+    # print("[STATUS] Started extracting  textures..")
+    # i = 1
+    # Liso=0
+    # Rugoso=0
+    # Acertos = 0
+    # Erros = 0
+    # Amostras = 0
+    # for train_name in train_names:
+    #     cur_path = train_path + "/" + train_name
+    #     cur_label = train_name
+    #     for file in glob.glob(cur_path):
+    #         print ("Processing Image - {} in {}".format(i, cur_label))
+    #         features = TrataImagem(file)
+    #         # append the feature vector and label
+    #         train_features.append(features[2])
+    #         train_labels.append(cur_label[5])
+    #         if(cur_label[5]=='L'):
+    #             Liso +=1
+    #         elif(cur_label[5]=='R'):
+    #             Rugoso+=1
+    #         # show loop update
+    #         i += 1
+    # # have a look at the size of our feature vector and labels
+    # print ("Training features: {}".format(np.array(train_features).shape))
+    # print ("Training labels: {}".format(np.array(train_labels).shape))
+    # print("Liso:",Liso)
+    # print("Rugoso:", Rugoso)
 
-    # empty list to hold feature vectors and train labels
-    train_features = []
-    train_labels = []
+    train = pd.read_csv('Train.csv',index_col='Object')
+    test = pd.read_csv('Test.csv', index_col='Object')
+    Texture = train.columns[1:3]
+    TextureLabel = train['TextureLabel']
+    TextureLabelTest=test['TextureLabel']
+    le = LabelEncoder()
+    le.fit(TextureLabelTest)
+    TextureLabelTest = le.transform(TextureLabelTest)
+    le.fit(TextureLabel)
+    TextureLabel = le.transform(TextureLabel)
+    X_train=train[Texture]
 
-    # loop over the training dataset
-    print("[STATUS] Started extracting  textures..")
-    i = 1
-    Liso=0
-    Rugoso=0
-    Acertos = 0
-    Erros = 0
-    Amostras = 0
-    for train_name in train_names:
-        cur_path = train_path + "/" + train_name
-        cur_label = train_name
-        for file in glob.glob(cur_path):
-            print ("Processing Image - {} in {}".format(i, cur_label))
-            features = TrataImagem(file)
-            # append the feature vector and label
-            train_features.append(features[2])
-            train_labels.append(cur_label[5])
-            if(cur_label[5]=='L'):
-                Liso +=1
-            elif(cur_label[5]=='R'):
-                Rugoso+=1
-            # show loop update
-            i += 1
-    # have a look at the size of our feature vector and labels
-    print ("Training features: {}".format(np.array(train_features).shape))
-    print ("Training labels: {}".format(np.array(train_labels).shape))
-    print("Liso:",Liso)
-    print("Rugoso:", Rugoso)
-
-
-    """ SVM """
-     # create the SVM classifier
-    print ("[STATUS] Creating the classifier..")
-    clf_svm = SVC(C=0.065 ,gamma=0.5,class_weight='balanced')
-
+    # svm = SVC()
+    # parameters = {'kernel': ('linear', 'rbf'), 'C': (1, 0.25, 0.5, 0.75,0.05), 'gamma': (0.5,1, 2, 3, 'auto'),
+    #               'decision_function_shape': ('ovo', 'ovr'),'class_weight': [{0: 1,1: w2} for w2 in [2, 4, 6, 10,12]]}
+    # clf = GridSearchCV(svm, parameters,verbose = 2)
+    # clf.fit(X_train, TextureLabel)
+    # print("accuracy:" + str(np.average(cross_val_score(clf, X_train, TextureLabel, scoring='accuracy'))))
+    # print("f1:" + str(np.average(cross_val_score(clf, X_train, TextureLabel, scoring='f1'))))
+    # print(clf.best_params_)
+    # """ SVM """
+    #  # create the SVM classifier
+    # print ("[STATUS] Creating the classifier..")
+    # # clf_svm = SVC(C=0.065 ,gamma=0.5,class_weight='balanced')
+    clf_svm = SVC(C=1, gamma=0.5, class_weight={0:1,1:2},decision_function_shape = 'ovo')
     # fit the training data and labels
     print ("[STATUS] Fitting data/label to model..")
-    clf_svm.fit(train_features, train_labels)
+    clf_svm.fit(train[Texture], TextureLabel)
+    prediction = clf_svm.predict(test[Texture])
+    print("Accuracy for SVM on CV data: ", accuracy_score(TextureLabelTest, prediction))
 
     # loop over the test images
-    test_path = "/home/ellengiacometti/PycharmProjects/TCCFRUIT/PIC_LM_TEST"
-    for file in glob.glob(test_path + "/*.jpg"):
-        features = TrataImagem(file)
-
-        # evaluate the model and predict label
-        features = np.array(features[2])
-        prediction = clf_svm.predict(features.reshape(1, -1))[0]
-
-        print("Nome:", file[58:70])
-        print("Prediction:", prediction)
-        Amostras+=1
-        if(file[65]==prediction):
-            Acertos+=1
-        else:
-            Erros+=1
-
-    print('Numero de Acertos', Acertos)
-    print('Numero de Erros', Erros)
-    print('Numero de Amostras', Amostras)
-    print('%ERROS:', (Erros/Amostras)*100)
-    print('%ACERTOS:', (Acertos / Amostras) * 100)
+    # test_path = "/home/ellengiacometti/PycharmProjects/TCCFRUIT/PIC_LM_TEST"
+    # for file in glob.glob(test_path + "/*.jpg"):
+    #     features = TrataImagem(file)
+    #
+    #     # evaluate the model and predict label
+    #     features = np.array(features[2])
+    #     prediction = clf_svm.predict(features.reshape(1, -1))[0]
+    #
+    #     print("Nome:", file[58:70])
+    #     print("Prediction:", prediction)
+    #     Amostras+=1
+    #     if(file[65]==prediction):
+    #         Acertos+=1
+    #     else:
+    #         Erros+=1
+    # print('Numero de Acertos', Acertos)
+    # print('Numero de Erros', Erros)
+    # print('Numero de Amostras', Amostras)
+    # print('%ERROS:', (Erros/Amostras)*100)
+    # print('%ACERTOS:', (Acertos / Amostras) * 100)
 
     #
     # """ RANDOM FOREST """
