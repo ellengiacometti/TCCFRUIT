d3f1b5069ab3146349b8e505909589cdaf54c337
ellengiacometti <ellengiacometti@gmail.com>
2019-04-10 15:50:12 -0300

    Revert "Normalize para testar pcdanilo"
    This reverts commit ae068dcb

--- NormalizeCSV.py
+++ NormalizeCSV.py
@@ -6,7 +6,7 @@
 # https://medium.com/@rrfd/standardize-or-normalize-examples-in-python-e3f174b65dfc
 import numpy as np
 import pandas as pd
-
+from sklearn.preprocessing import normalize
 
 
 def create_columns(a):
@@ -17,54 +17,67 @@ def create_columns(a):
 
 ## ler data
 prop = lambda x: x.strip("[]").replace("'", "").split(", ")
-Names=['Train.csv','Test.csv']
-for Name in Names:
-    data = pd.read_csv(Name, index_col=False, sep=";",converters={'ColorH': prop, 'ColorS': prop, 'ColorV': prop})
+train = pd.read_csv('Train.csv', index_col=False, sep=";",converters={'ColorH': prop, 'ColorS': prop, 'ColorV': prop})
+## separar a info
+infoRegion = train.columns[1:9]
+info = train[infoRegion]
+infoMin=np.amin(info.values,axis=0)
+infoMax=np.amax(info.values,axis=0)
+num= info.values - infoMin
+den=infoMax-infoMin
+Norm_info= num/den
 
-    ## separar a info
-    infoRegion = data.columns[1:9]
-    info = data[infoRegion]
-    if Name== Names[0]:
-        infoMin=np.amin(info.values,axis=0)
-        infoMax=np.amax(info.values,axis=0)
-    num = info.values - infoMin
-    den = infoMax - infoMin
-    Norm_info = num / den
-    # print("Wait a minute....")
+# print("Wait a minute....")
 
-    infoRegionHists = data.columns[9:12]
-    infoHists=data[infoRegionHists]
-    # NormA_feat=[]
-    NormA_info=create_columns(3)
-    featHist=[]
+infoRegionHists = train.columns[9:12]
+infoHists=train[infoRegionHists]
+# NormA_feat=[]
+NormA_info=create_columns(3)
+featHist=[]
+NormA_feat = []
+n=0
+for i in infoRegionHists:
+    infoHist = infoHists[i]
+    featHist = infoHist.values
+    for hist in featHist:
+        hist=np.float64(hist)
+        histMax=np.amax(hist,axis=0)
+        histNorm=hist/histMax
+        NormA_feat.append(histNorm.tolist())
+
+    # corH=pd.Series(NormA_feat,index=['ColorH'])
+    NormA_info[n] = NormA_feat
     NormA_feat = []
-    n=0
-    for i in infoRegionHists:
-        infoHist = infoHists[i]
-        featHist = infoHist.values
-        for hist in featHist:
-            hist=np.float64(hist)
-            histMax=np.amax(hist,axis=0)
-            histNorm=hist/histMax
-            NormA_feat.append(histNorm.tolist())
+    n += 1
+    print("Wait a minute....")
 
-        # corH=pd.Series(NormA_feat,index=['ColorH'])
-        NormA_info[n] = NormA_feat
-        NormA_feat = []
-        n += 1
-        print("Wait a minute....")
-
-    raw_data = {'Object': data['Object'], 'Kurtosis': Norm_info[:,0].tolist(), 'Skewness': Norm_info[:,1].tolist(),
-                'Dissimilarity': Norm_info[:,2].tolist(), 'Correlation': Norm_info[:,3].tolist(), 'Homogeneity': Norm_info[:,4].tolist(),
-                'Energy': Norm_info[:,5].tolist(), 'Contrast': Norm_info[:,6].tolist(), 'ASM': Norm_info[:,7].tolist(),
-                'ColorH':NormA_info[0],'ColorS':NormA_info[1],'ColorV':NormA_info[2],
-                'TextureLabel':data['TextureLabel'],'ColorLabel':data['ColorLabel'],'Radius':data['Radius']}
-    normA = pd.DataFrame(raw_data,columns=data.columns.values)
-    fileName= 'Norm'+ Name.strip('.csv')+str(data.shape[0])+'.csv'
-normA.to_csv(fileName, index=False, sep=";")
-print(fileName," CRIADO!")
+raw_data = {'Object': train['Object'], 'Kurtosis': Norm_info[:,0].tolist(), 'Skewness': Norm_info[:,1].tolist(),
+            'Dissimilarity': Norm_info[:,2].tolist(), 'Correlation': Norm_info[:,3].tolist(), 'Homogeneity': Norm_info[:,4].tolist(),
+            'Energy': Norm_info[:,5].tolist(), 'Contrast': Norm_info[:,6].tolist(), 'ASM': Norm_info[:,7].tolist(),
+            'ColorH':NormA_info[0],'ColorS':NormA_info[1],'ColorV':NormA_info[2],
+            'TextureLabel':train['TextureLabel'],'ColorLabel':train['ColorLabel'],'Radius':train['Radius']}
+normA = pd.DataFrame(raw_data,columns=train.columns.values)
+normA.to_csv('NormTrain658.csv', index=False, sep=";")
+print("NormTrain658.csv CRIADO")
 
 parameterData= {'Feature': infoRegion.T, 'Max':infoMax,'Min':infoMin }
 Data = pd.DataFrame(parameterData,columns=['Feature','Max','Min'])
 Data.to_csv('Data_NormTrain658.csv', index=False, sep=";")
 print("Data_NormTrain658.csv CRIADO")
+#
+# raw_dataHist = {'Object': train['Object'], 'ColorH':NormA_info[8] }
+# normAHist = pd.DataFrame(raw_dataHist, columns=['Object','ColorH'])
+# normAHist.to_csv('COLORA.csv', index=False, sep=";")
+# raw_dataHist = {'Object': train['Object'], 'ColorH':NormB_info[8]}
+# normBHist = pd.DataFrame(raw_dataHist, columns=['Object','ColorH'])
+# normBHist.to_csv('COLORB.csv', index=False, sep=";")
+
+# raw_data = {'Object': train['Object'], 'Kurtosis': NormB_info[0], 'Skewness': NormB_info[1],
+#             'Dissimilarity': NormB_info[2], 'Correlation': NormB_info[3], 'Homogeneity': NormB_info[4],
+#             'Energy': NormB_info[5], 'Contrast': NormB_info[6], 'ASM': NormB_info[7],
+#             'ColorH':NormB_info[8],'ColorS':NormB_info[9],'ColorV':NormB_info[10],
+#             'TextureLabel':train['TextureLabel'],'ColorLabel':train['ColorLabel'] }
+#
+# normB = pd.DataFrame(raw_data,columns=train.columns.values)
+# normB.to_csv('normBTrain.csv', index=False, sep=";")
+# print("normBTrain.csv CRIADO")
--- RandomForest.py
+++ RandomForest.py
@@ -31,85 +31,86 @@ if __name__ == '__main__':
     le.fit(ColorLabelTrain)
     ColorLabelTrain = le.transform(ColorLabelTrain)
 
-    # ColorTestH = [list(map(float, histH)) for histH in test['ColorH']]
-    # ColorTestH = np.array(ColorTestH)
-    # ColorTestS = [list(map(float, histS)) for histS in test['ColorS']]
-    # ColorTestS = np.array(ColorTestS)
-    # ColorTestV = [list(map(float, histV)) for histV in test['ColorV']]
-    # ColorTestV = np.array(ColorTestV)
-    # colunasTest = test.columns[1:9]
-    # TextureTest = test[colunasTest].values
-    # colunaTestColor = np.hstack((ColorTestH, ColorTestS, ColorTestV))
-    # FeaturesTest = np.hstack((TextureTest, colunaTestColor))
-    # TextureLabelTest = test['TextureLabel']
-    # le.fit(TextureLabelTest)
-    # TextureLabelTest = le.transform(TextureLabelTest)
-    # ColorLabelTest = test['ColorLabel']
-    # le.fit(ColorLabelTest)
-    # ColorLabelTest = le.transform(ColorLabelTest)
-
-    # # #Create a based model
+    ColorTestH = [list(map(float, histH)) for histH in test['ColorH']]
+    ColorTestH = np.array(ColorTestH)
+    ColorTestS = [list(map(float, histS)) for histS in test['ColorS']]
+    ColorTestS = np.array(ColorTestS)
+    ColorTestV = [list(map(float, histV)) for histV in test['ColorV']]
+    ColorTestV = np.array(ColorTestV)
+    colunasTest = test.columns[1:9]
+    TextureTest = test[colunasTest].values
+    colunaTestColor = np.hstack((ColorTestH, ColorTestS, ColorTestV))
+    FeaturesTest = np.hstack((TextureTest, colunaTestColor))
+    TextureLabelTest = test['TextureLabel']
+    le.fit(TextureLabelTest)
+    TextureLabelTest = le.transform(TextureLabelTest)
+    ColorLabelTest = test['ColorLabel']
+    le.fit(ColorLabelTest)
+    ColorLabelTest = le.transform(ColorLabelTest)
+    # #Create a based model
     # rf = RandomForestClassifier(random_state=42)
     # # Create the parameter grid based on the results of random search
     # param_grid = {
     #     'n_estimators': [10, 20, 30, 40,50,60,70,80,90,100],
     #     'class_weight': [{0: 1,1: w2} for w2 in [2, 4, 6, 10,12]]
     #
-    param_grid = {
-        # Number of trees in random forest
-        'n_estimators': [int(x) for x in np.linspace(start=20, stop=200, num=10)],
-         # Number of features to consider at every split
-        'max_features': ['auto', 'sqrt'],
-         # Maximum number of levels in tree
-        'max_depth': [int(x) for x in np.linspace(10, 30, num=11)],
-         # Minimum number of samples required to split a node
-        'min_samples_split': [2, 5, 10],
-         # Minimum number of samples required at each leaf node
-        'min_samples_leaf': [1, 2, 4],
-        # Method of selecting samples for training each tree
-        'bootstrap': [True, False]
-    }
+    # param_grid = {
+    #     # Number of trees in random forest
+    #     'n_estimators': [int(x) for x in np.linspace(start=20, stop=2000, num=10)],
+    #      # Number of features to consider at every split
+    #     'max_features': ['auto', 'sqrt'],
+    #      # Maximum number of levels in tree
+    #     'max_depth': [int(x) for x in np.linspace(10, 30, num=11)],
+    #      # Minimum number of samples required to split a node
+    #     'min_samples_split': [2, 5, 10,15],
+    #      # Minimum number of samples required at each leaf node
+    #     'min_samples_leaf': [1, 2, 4,6],
+    #     # Method of selecting samples for training each tree
+    #     'bootstrap': [True, False]
+    # }
     # # Create a based model
-    rf = RandomForestClassifier()
-    # Instantiate the grid search model
-    grid_search = GridSearchCV(estimator=rf, param_grid=param_grid,
-                               cv=3, n_jobs=-1, verbose=100)
-
+    # rf = RandomForestClassifier()
     # # Instantiate the grid search model
-    CV_rfc = GridSearchCV(estimator = rf, param_grid = param_grid,cv = 5, n_jobs = -1, verbose = 60)
-    # Fit the grid search to the data
-    CV_rfc.fit(FeaturesTrain,ColorLabelTrain)
-    CV_rfc.best_params_
-    print(CV_rfc.best_params_)
-#
+    # grid_search = GridSearchCV(estimator=rf, param_grid=param_grid,
+    #                            cv=3, n_jobs=-1, verbose=60)
+    # # }
+    # # # Instantiate the grid search model
+    # CV_rfc = GridSearchCV(estimator = rf, param_grid = param_grid,cv = 5, n_jobs = -1, verbose = 60)
+    # # Fit the grid search to the data
+    # CV_rfc.fit(FeaturesTrain,TextureLabelTrain)
+    # CV_rfc.best_params_
+    # print(CV_rfc.best_params_)
+##ACHADO DURANTE O PROCESSO
+# max_depth=18, min_samples_leaf=1, min_samples_split=2, bootstrap=True, max_features=sqrt, n_estimators=20(97%)
+    print("~~~ INFO BASE TESTE ~~~")
+    print("NÚMERO RUGOSOS:", sum(TextureLabelTest))
+    print("NÚMERO LISOS:", len(TextureLabelTest) - sum(TextureLabelTest))
+    print("NÚMERO SEM DEFEITO:", sum(ColorLabelTest))
+    print("NÚMERO COM DEFEITO:", len(ColorLabelTest) - sum(ColorLabelTest))
+    print("~~~ INFO BASE TREINO ~~~")
+    print("NÚMERO RUGOSOS:", sum(TextureLabelTrain))
+    print("NÚMERO LISOS:", len(TextureLabelTrain) - sum(TextureLabelTrain))
+    print("NÚMERO SEM DEFEITO:", sum(ColorLabelTrain))
+    print("NÚMERO COM DEFEITO:", len(ColorLabelTrain) - sum(ColorLabelTrain))
 
-#     print("~~~ INFO BASE TESTE ~~~")
-#     print("NÚMERO RUGOSOS:", sum(TextureLabelTest))
-#     print("NÚMERO LISOS:", len(TextureLabelTest) - sum(TextureLabelTest))
-#     print("NÚMERO SEM DEFEITO:", sum(ColorLabelTest))
-#     print("NÚMERO COM DEFEITO:", len(ColorLabelTest) - sum(ColorLabelTest))
-#     print("~~~ INFO BASE TREINO ~~~")
-#     print("NÚMERO RUGOSOS:", sum(TextureLabelTrain))
-#     print("NÚMERO LISOS:", len(TextureLabelTrain) - sum(TextureLabelTrain))
-#     print("NÚMERO SEM DEFEITO:", sum(ColorLabelTrain))
-#     print("NÚMERO COM DEFEITO:", len(ColorLabelTrain) - sum(ColorLabelTrain))
+    print("\n~~~ RANDOM FOREST -  CLASSIFICADOR LISO X RUGOSO ~~~")
+    #print("[STATUS] Creating the classifier..")
+    clf_rfLR=RandomForestClassifier(max_depth=40, min_samples_leaf=1, min_samples_split=2, bootstrap=False, max_features='sqrt', n_estimators=20)
+    #print("[STATUS] Fitting data/label to model..")
+    clf_rfLR.fit(FeaturesTrain, TextureLabelTrain)
+    #print("[STATUS] Predicting Trained DataBase..")
+    pred=clf_rfLR.predict(FeaturesTest)
+    print("Accuracy for Random Forest on TEST data: ",accuracy_score(TextureLabelTest,pred))
+    print("Confusion Matrix: ", confusion_matrix(TextureLabelTest, pred))
 
-    # print("\n~~~ RANDOM FOREST -  CLASSIFICADOR LISO X RUGOSO ~~~")
-    # #print("[STATUS] Creating the classifier..")
-    # clf_rfLR=RandomForestClassifier('n_estimators': 100, 'min_samples_split': 2, 'max_features': 'auto', 'max_depth': 24, 'min_samples_leaf': 1, 'bootstrap': False)
-    # #print("[STATUS] Fitting data/label to model..")
-    # clf_rfLR.fit(FeaturesTrain, TextureLabelTrain)
-    # #print("[STATUS] Predicting Trained DataBase..")
-    # pred=clf_rfLR.predict(FeaturesTest)
-    # print("Accuracy for Random Forest on TEST data: ",accuracy_score(TextureLabelTest,pred))
-    # print("Confusion Matrix: ", confusion_matrix(TextureLabelTest, pred))
+    print("\n~~~ RANDOM FOREST -  CLASSIFICADOR COM DEFEITO X SEM DEFEITO ~~~")
+    #print("[STATUS] Creating the classifier..")
 
-    # print("\n~~~ RANDOM FOREST -  CLASSIFICADOR COM DEFEITO X SEM DEFEITO ~~~")
-    # clf_rfCS = RandomForestClassifier(max_depth=40, min_samples_leaf=1, min_samples_split=2, bootstrap=False, max_features='sqrt', n_estimators=20)
-    # #print("[STATUS] Fitting data/label to model..")
-    # clf_rfCS.fit(FeaturesTrain, ColorLabelTrain)
-    # #print("[STATUS] Predicting Trained DataBase..")
-    # pred = clf_rfCS.predict(FeaturesTest)
-    # print("Accuracy for Random Forest on TEST data: ", accuracy_score(ColorLabelTest, pred))
-    # print("Confusion Matrix: ", confusion_matrix(ColorLabelTest, pred))
+    clf_rfCS = RandomForestClassifier(max_depth=40, min_samples_leaf=1, min_samples_split=2, bootstrap=False, max_features='sqrt', n_estimators=20)
+    #print("[STATUS] Fitting data/label to model..")
+    clf_rfCS.fit(FeaturesTrain, ColorLabelTrain)
+    #print("[STATUS] Predicting Trained DataBase..")
+    pred = clf_rfCS.predict(FeaturesTest)
+    print("Accuracy for Random Forest on TEST data: ", accuracy_score(ColorLabelTest, pred))
+    print("Confusion Matrix: ", confusion_matrix(ColorLabelTest, pred))
 
